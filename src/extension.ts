5905fe48e43d1edd9cb9f13efca76edd813cb1d6
trent <trent.hilliam@vorteil.io>
2021-08-02 13:35:21 +1000

    Fixed a problem with local development

--- src/extension.ts
+++ src/extension.ts
@@ -56,6 +56,7 @@ export function activate(context: vscode.ExtensionContext) {
 		https.get(`${dlurl}${binName}`, (resp:any)=>{
 			console.log('piping')
 			resp.pipe(fs.createWriteStream(direktionPath))
+            fs.chmodSync(direktionPath, 0o755)
 		})
 	}
 
